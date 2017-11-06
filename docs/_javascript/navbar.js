5325878cb6bdcb18f967d9e052b8d03d0361234a
Jeremy Thomas <bbxdesign@gmail.com>
2017-11-06 12:56:05 +0000

    Remove fix top navbar toggle

--- docs/_javascript/navbar.js
+++ docs/_javascript/navbar.js
@@ -3,28 +3,9 @@ document.addEventListener('DOMContentLoaded', () => {
   const rootEl = document.documentElement;
   const navbarTopEl = document.getElementById('navbar');
   const navbarBottomEl = document.getElementById('navbarBottom');
-  const fixTopEl = document.getElementById('navbarFixTop');
-  const fixTopElIcon = fixTopEl.querySelector('.fa');
   const fixBottomEl = document.getElementById('navbarFixBottom');
   const fixBottomElIcon = fixBottomEl.querySelector('.fa');
-  let fixedTop = false;
   let fixedBottom = false;
-
-  fixTopEl.addEventListener('click', event => {
-    fixedTop = !fixedTop;
-
-    if (fixedTop) {
-      fixTopEl.className = 'button is-success';
-      fixTopElIcon.className = 'fa fa-check-square-o';
-      rootEl.classList.add('has-navbar-fixed-top');
-      navbarTopEl.classList.add('is-fixed-top', 'has-shadow');
-    } else {
-      fixTopEl.className = 'button is-link';
-      fixTopElIcon.className = 'fa fa-square-o';
-      rootEl.classList.remove('has-navbar-fixed-top');
-      navbarTopEl.classList.remove('is-fixed-top', 'has-shadow');
-    }
-  });
 
   fixBottomEl.addEventListener('click', event => {
     fixedBottom = !fixedBottom;
--- docs/lib/navbar.js
+++ docs/lib/navbar.js
@@ -5,28 +5,9 @@ document.addEventListener('DOMContentLoaded', function () {
   var rootEl = document.documentElement;
   var navbarTopEl = document.getElementById('navbar');
   var navbarBottomEl = document.getElementById('navbarBottom');
-  var fixTopEl = document.getElementById('navbarFixTop');
-  var fixTopElIcon = fixTopEl.querySelector('.fa');
   var fixBottomEl = document.getElementById('navbarFixBottom');
   var fixBottomElIcon = fixBottomEl.querySelector('.fa');
-  var fixedTop = false;
   var fixedBottom = false;
-
-  fixTopEl.addEventListener('click', function (event) {
-    fixedTop = !fixedTop;
-
-    if (fixedTop) {
-      fixTopEl.className = 'button is-success';
-      fixTopElIcon.className = 'fa fa-check-square-o';
-      rootEl.classList.add('has-navbar-fixed-top');
-      navbarTopEl.classList.add('is-fixed-top', 'has-shadow');
-    } else {
-      fixTopEl.className = 'button is-link';
-      fixTopElIcon.className = 'fa fa-square-o';
-      rootEl.classList.remove('has-navbar-fixed-top');
-      navbarTopEl.classList.remove('is-fixed-top', 'has-shadow');
-    }
-  });
 
   fixBottomEl.addEventListener('click', function (event) {
     fixedBottom = !fixedBottom;
