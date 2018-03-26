7c9f0a76dc094d66442eee41fb0fb7b93ec4c9a6
Jeremy Thomas <bbxdesign@gmail.com>
2018-03-26 14:46:39 +0100

    Add Bulma book

--- docs/lib/navbar.js
+++ docs/lib/navbar.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', function () {
   var navbarTopEl = document.getElementById('navbar');
   var navbarBottomEl = document.getElementById('navbarBottom');
   var fixBottomEl = document.getElementById('navbarFixBottom');
-  var fixBottomElIcon = fixBottomEl.querySelector('.fa');
+  var fixBottomElText = document.getElementById('navbarFixBottomText');
   var fixedBottom = false;
 
   fixBottomEl.addEventListener('click', function (event) {
@@ -14,12 +14,12 @@ document.addEventListener('DOMContentLoaded', function () {
 
     if (fixedBottom) {
       fixBottomEl.className = 'button is-success';
-      fixBottomElIcon.className = 'far fa-check-square';
+      fixBottomElText.innerHTML = 'Hide';
       rootEl.classList.add('has-navbar-fixed-bottom');
       navbarBottomEl.classList.remove('is-hidden');
     } else {
       fixBottomEl.className = 'button is-link';
-      fixBottomElIcon.className = 'far fa-square';
+      fixBottomElText.innerHTML = 'Show';
       rootEl.classList.remove('has-navbar-fixed-bottom');
       navbarBottomEl.classList.add('is-hidden');
     }
