7442c980e53bdcbbd8ac11daa313d834e0788f14
Jeremy Thomas <bbxdesign@gmail.com>
2018-06-16 19:01:35 +0100

    Read derived variables

--- docs/scripts/variables.js
+++ docs/scripts/variables.js
@@ -6,18 +6,21 @@ var writemetadata = require('metalsmith-writemetadata');
 // var regex = '**/*.sass';
 var regex_initial = '**/initial-variables.sass';
 var regex_derived = '**/derived-variables.sass';
-var initial_plugin = require('./read-initial-variables');
-var derived_plugin = require('./read-derived-variables');
+var initial_plugin = require('./01-read-initial-variables');
+var derived_plugin = require('./02-read-derived-variables');
 
 Metalsmith(__dirname)
-  .source('../../sass/utilities')
-  // .destination('./output')
-  // .clean(true)
+  .source('../../sass')
   .use(filter(regex_initial))
   .use(initial_plugin())
-  // .use(filter(regex_derived))
-  // .use(derived_plugin())
-  // .use(writemetadata())
+  .build(function(err) {
+    if (err) throw err;
+  });
+
+Metalsmith(__dirname)
+  .source('../../sass')
+  .use(filter(regex_derived))
+  .use(derived_plugin())
   .build(function(err) {
     if (err) throw err;
   });
