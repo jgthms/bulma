238b43761fa582dbe33b378e2dc90fefa0d97119
Jeremy Thomas <bbxdesign@gmail.com>
2020-09-06 10:28:48 +0100

    Add CSS var map regex, Fix basic list of CSS vars

--- test/plugins/check-css-variables-exist.js
+++ test/plugins/check-css-variables-exist.js
@@ -2,14 +2,25 @@ module.exports = plugin;
 
 const utils = require('./utils');
 const fs = require('fs');
+const regexRegisterColors = /registerCSSVarColors\((\$[a-z]+)\)/g;
 const regexAssign = /--[a-z-]*:/g;
 const regexUsage = /var\(--[a-z-]*(\, \#|\))/g;
 const LOG_EVERYTHING = false;
+
+const COLOR_MAPS = {
+  '$colors': ["white", "black", "light", "dark", "primary", "link", "info", "success", "warning", "danger"],
+  '$shades': ["black-bis", "black-ter", "grey-darker", "grey-dark", "grey", "grey-light", "grey-lighter", "white-ter", "white-bis"],
+}
 
 function logThis(message) {
   if (LOG_EVERYTHING) {
     console.log(message);
   }
+}
+
+function getFirstGroup(regexp, str) {
+  const array = [...str.matchAll(regexp)];
+  return array.map(m => m[1]);
 }
 
 function plugin() {
@@ -18,9 +29,27 @@ function plugin() {
 
     let hasErrors = false;
 
-    const cssvars = fs.readFileSync(`../sass/themes/default.sass`, "utf8");
-    let defaultAssignments = cssvars.match(regexAssign);
+    const cssvars = fs.readFileSync(`../sass/themes/basic.sass`, "utf8");
+
+    // Check for --cyan: #{$cyan}
+    let defaultAssignments = cssvars.match(regexAssign) || [];
     defaultAssignments = defaultAssignments.map(assignment => assignment.replace(':', ''));
+
+    // Check for +registerCSSVarColors($colors)
+    let registerMaps = getFirstGroup(regexRegisterColors, cssvars);
+
+    if (registerMaps) {
+      registerMaps.forEach(mapName => {
+        if (mapName in COLOR_MAPS) {
+          const colors = COLOR_MAPS[mapName].map(colorName => {
+            defaultAssignments.push(`--${colorName}`);
+            defaultAssignments.push(`--${colorName}-invert`);
+            defaultAssignments.push(`--${colorName}-light`);
+            defaultAssignments.push(`--${colorName}-dark`);
+          });
+        }
+      });
+    }
 
     Object.keys(files).forEach(filePath => {
       const {fileName, lines} = utils.getLines(files, filePath);
